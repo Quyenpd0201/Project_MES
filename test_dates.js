99f129b9a3389a856576f3327dfbf699297c8834
xone184 <mvtho2004@gmail.com>
2026-07-27 15:23:35 +0700

    chore: clean up unnecessary files and scratchpads

--- test_dates.js
+++
@@ -1,5 +0,0 @@
-const d = (ymd) => new Date(ymd + "T00:00:00");
-const toYMD = (date) => `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
-const addDays = (ymd, n) => { const x = d(ymd); x.setDate(x.getDate() + n); return toYMD(x); };
-
-console.log(addDays('2026-07-06', -7));
